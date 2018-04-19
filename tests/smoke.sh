b6e3050ffb5792e42517fe88e49f0a2712ddf3fb
Rafael Ostertag <rafi@guengel.ch>
2018-04-19 22:06:09 +0200

    Added smoke test.

---
+++ tests/smoke.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#
+# Smoke tests.
+
+set -eu
+
+
+trap '{ docker-compose --project-name smoke-test -f docker-compose.yml down ; }' EXIT
+
+docker-compose --project-name smoke-test -f docker-compose.yml up -d
+
+echo "Wait for healthy anath container..."
+
+for i in {1..8}
+do
+    sleep 15
+    echo "#$i: Test health of anath container"
+    if docker ps -f name=smoke-test_anath-server_1 --format '{{ .Status }}' | grep 'healthy' >/dev/null 2>&1
+    then
+	echo "Anath container is in healthy state"
+	exit 0
+    fi
+
+done
+echo "Anath container is not in healthy state after 120secs" >&2
+exit 1
