cee3367eb0c8dc41fdccb964d1a5845d88ac026a
Rafael Ostertag <rafael.ostertag@datonus.ch>
2018-04-20 15:24:09 +0200

    Docker compose files can be passed as command line arguments.

--- tests/smoke.sh
+++ tests/smoke.sh
@@ -1,20 +1,47 @@
 #!/bin/bash
 #
-# Smoke tests.
+# Anath docker image smoke test.
+#
+# This script expects the CWD to be the repository root.
+#
+# Provide the docker compose file and overrides as arguments to the script, e.g.
+#
+#  smoke.sh docker-compose.yml docker-compose-other.yml
+#
+# When no docker compose file is specified, the default is taken by docker-compose.
 
 set -eu
 
-trap '{ docker-compose --project-name smoke-test -f docker-compose.yml down ; }' EXIT
+# Transform the values in "$@" to
+#   "-f $1 ... -f $N"
+function get_docker_compose_file_arguments() {
+    local args=""
+    
+    for f in "$@"
+    do
+	args="${args} -f $f"
+    done
 
-docker-compose --project-name smoke-test -f docker-compose.yml up -d
+    echo $args
+}
+
+SLEEP=${SLEEP:-15}
+TRIES=${TRIES:-8}
+PROJECT_NAME=${PROJECT_NAME:-smoke-test}
+ANATH_SERVER_NAME=${ANATH_SERVER_NAME:-anath-server}
+DOCKER_FILE_ARGS=`get_docker_compose_file_arguments "$@"`
+
+trap "{ docker-compose --project-name \"${PROJECT_NAME}\" ${DOCKER_FILE_ARGS} down ; }" EXIT
+
+docker-compose --project-name "${PROJECT_NAME}" ${DOCKER_FILE_ARGS} up -d
 
 echo "Wait for healthy anath container..."
 
-for i in {1..8}
+for i in `seq 1 ${TRIES}`
 do
     sleep 15
-    echo "#$i: Test health of anath container"
-    if docker ps -f name=smoke-test_anath-server_1 --format '{{ .Status }}' | grep 'healthy' >/dev/null 2>&1
+    echo "#$i: Is anath container healthy?"
+    if docker ps -f name="${PROJECT_NAME}_${ANATH_SERVER_NAME}_1" --format '{{ .Status }}' | grep 'healthy' >/dev/null 2>&1
     then
 	echo "Anath container is in healthy state"
 	exit 0
@@ -24,5 +51,5 @@ done
 echo "Current state"
 docker ps -a
 
-echo "Anath container is not in healthy state after 120secs" >&2
+echo "Anath container is not in healthy state after $((SLEEP * TRIES))secs" >&2
 exit 1
